77c9d4cb145be0de714d9a152379ce481fd10547
Samantha Callie <Hoomee90@gmail.com>
2023-08-04 14:49:56 -0700

    add js button functionality to light/dark mode buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
 // User Interface Logic
+
 window.onload = function() {
- 
+  const body = document.querySelector("body");
+  const darkButton = document.getElementById("option2");
+  const darkContainer = darkButton.parentElement;
+  const lightButton = document.getElementById("option1")
+  const lightContainer = lightButton.parentElement;
+
+  darkButton.onclick = function() {
+    body.style.backgroundColor = "dimgray";
+
+    lightContainer.classList.remove("active");
+    darkContainer.classList.add("active");
+  };
+
+  lightButton.onclick = function() {
+    body.style.backgroundColor = "white";
+
+    darkContainer.classList.remove("active");
+    lightContainer.classList.add("active");
+  };
 };
