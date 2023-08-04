96cb405d91525f6280a67efbff062362dce076b0
Samantha Callie <Hoomee90@gmail.com>
2023-08-04 15:40:17 -0700

    refactor js and enable card background to change

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,37 @@
 
 window.onload = function() {
   const body = document.querySelector("body");
+
   const darkButton = document.getElementById("option2");
   const darkContainer = darkButton.parentElement;
   const lightButton = document.getElementById("option1")
   const lightContainer = lightButton.parentElement;
 
-  darkButton.onclick = function() {
-    body.style.backgroundColor = "dimgray";
+  classExchange = function (receiver, giver, toGive, toReceive) {
+    giver.classList.remove(toGive)
+    receiver.classList.add(toReceive)
+    console.log("exchange completed!")
+  };
 
-    lightContainer.classList.remove("active");
-    darkContainer.classList.add("active");
+  darkButton.onclick = function() {
+    let lightCards = document.querySelectorAll(".bg-light");
+   
+    classExchange(body, body, "bg-light", "bg-dark")
+    classExchange(darkContainer, lightContainer, "active", "active")
+
+    lightCards.forEach(card => {
+      classExchange(card, card, "bg-light", "bg-dark")
+    });
   };
 
   lightButton.onclick = function() {
-    body.style.backgroundColor = "white";
+    let darkCards = document.querySelectorAll(".bg-dark");
+   
+    classExchange(body, body, "bg-dark", "bg-light")
+    classExchange(lightContainer, darkContainer, "active", "active")
 
-    darkContainer.classList.remove("active");
-    lightContainer.classList.add("active");
+    darkCards.forEach(card => {
+      classExchange(card, card, "bg-dark", "bg-light")
+    });
   };
 };
